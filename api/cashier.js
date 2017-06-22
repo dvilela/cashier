0e4b94dc0cd616ca7dfe3c2c46481fba78f8a592
Dênis Vilela <denisxvilela@gmail.com>
2017-06-21 23:06:42 -0300

    API authentication

--- api/cashier.js
+++ api/cashier.js
@@ -7,23 +7,67 @@ const morgan = require('./helpers/morgan');
 const middleware = require('./middleware');
 const initializeDB = require('./db');
 const routers = require('./routers');
+const expressJWT = require('express-jwt');
+const jwt = require('jsonwebtoken');
 
 Object.assign(config, require('./helpers/api-details')(config.api));
 
-logger.debug('Starting cashier', { config, env } );
+logger.debug('Starting cashier', { config, env });
 
 const app = express();
 
 // enable CORS
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   next();
 });
 
-// 3rd party middleware
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+/* Temp code lines that have to be moved (code beauty reasons) */
+const fs = require('fs');
+const SECRET = fs.readFileSync(require('path').resolve('API_SECRET'));
+// Security with JWT
+if (SECRET != null) {
+  app.use(expressJWT({ secret: SECRET }).unless({ path: ['/login', { url: '/api/v1/users', methos: 'POST' }] }));
+  // login
+  const validateUser = (req, res, next) => {
+    if (req.body.username == null) {
+      res.status(400).send('Username Required');
+      return;
+    }
+    if (req.body.password == null) {
+      res.status(400).send('Password Required');
+      return;
+    }
+    next();
+  };
+  const User = require('./models/User');
+  app.post('/login', validateUser);
+  app.post('/login', (req, res) => {
+    User.findOne({ username: req.body.username }, (err, user) => {
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        if (err) throw err;
+        if (!isMatch) {
+          res.status(401).send('Invalid Password');
+        } else {
+          const myToken = jwt.sign({ username: req.body.username }, SECRET);
+          res.status(200).json(myToken);
+        }
+      });
+    });
+  });
+  app.post('/api/v1/users', validateUser);
+  app.post('/api/v1/users', (req, res) => {
+    User.create({ username: req.body.username, password: req.body.password })
+      .then(() => res.send(201));
+  });
+}
+
+// 3rd party middleware
 app.use(morgan);
 
 // internal middleware
--- api/models/helper.js
+++ api/models/helper.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt'),
+  SALT_WORK_FACTOR = 10;
 
 const createModel = (name, schema, options) => {
   if (options == null || options.timestamps == null) {
@@ -10,4 +12,43 @@ const createModel = (name, schema, options) => {
   return mongoose.model(name, new Schema(schema, options));
 }
 
-module.exports = { createModel, Schema };
+const createUserModel = () => {
+  var UserSchema = new Schema({
+    username: { type: String, required: true, index: { unique: true } },
+    password: { type: String, required: true }
+  }, {
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+  });
+
+  UserSchema.pre('save', function (next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+      });
+    });
+  });
+
+  UserSchema.methods.comparePassword = function (candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function (err, isMatch) {
+      if (err) return cb(err);
+      cb(null, isMatch);
+    });
+  };
+
+  return mongoose.model('User', UserSchema);
+}
+
+module.exports = { createModel, Schema, createUserModel };
